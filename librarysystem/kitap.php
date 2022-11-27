24affab47c8d03caf427f463a826c6c4cc04f92b
farukpinar97 <117291355+farukpinar97@users.noreply.github.com>
2022-11-27 19:20:34 +0300

    Add files via upload

--- librarysystem/kitap.php
+++ librarysystem/kitap.php
@@ -1,49 +1,54 @@
+<?php
+// veritabanina baglaniyoruz
+include('include/baglan.php'); ?>
+<?php
+if(isset($_POST['ara']) && $_POST['arama'] != '')
+{
+   $arananKelime = $_POST['arama'];
+   // veritabanindan arama yapiliyo
+   $stmt = $conn->query("SELECT * FROM kitap WHERE kitap_adi LIKE '%".$arananKelime."%' or yazar_adi LIKE '%".$arananKelime."%' ORDER BY tarih DESC");
+}
+else {
+    // veritabanindan tum kitaplari aliyoruz ve ekleme tarihine gore siraliyoruz. En son eklenenler en ustte gorunucek
+    $stmt = $conn->query("SELECT * FROM kitap ORDER BY tarih DESC");
+}
+?>
+
+<!doctype html>
 <html lang="tr">
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="x-ua-compatible" content="ie-edge">
-    <title>Kitap Listesi</title>
-    <link rel="stylesheet" href="css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/main.css">
-</head>
-<body>
+  <?php include 'include/header.php';?>
+
+  <body>
+
+      <?php include 'include/navigasyon.php';?>
+
+      <div class="container">
+
+          <?php if(isset($_POST['ara']) && $_POST['arama'] != '') { ?>
+                <div class="row"><span class="error-msg">Aranan kelime: <?php echo $_POST['arama']; ?> </span></div>
+          <?php } ?>
+
+          <div class="row">
+               <?php
+                // veritabanindan gelen kitaplari goruntuluyoruz
+                while ($row = $stmt->fetch()) { ?>
+                    <div class="col" style="padding: 10px 0;"><div class="card" style="width: 15rem;">
+                        <a href="kitap1.php?id=<?php echo $row['id'] ?>" title="<?php echo $row['kitap_adi']; ?>" ><img src="images/<?php echo $row['resim']; ?>" class="card-img-top" alt="..." /></a>
+                        <div class="card-body">
+                            <h5 class="card-title"><a href="kitap1.php?id=<?php echo $row['id'] ?>" title="<?php echo $row['kitap_adi']; ?>" ><?php echo $row['kitap_adi']; ?></a></h5>
+                            <p class="card-text"><?php echo $row['yazar_adi']; ?></p>
+                            <a href="#" class="btn btn-primary"><i class="fas fa-thumbs-up"></i></a>
+                            <a href="#" class="btn btn-primary"><i class="fas fa-thumbs-down"></i></a>
+                            <a href="#" class="btn btn-primary">Ödünç Al</a>
+                        </div>
+                    </div></div>
+               <?php } ?>
+          </div>
+      </div>
 
 
-<?php require_once "lib/db.php"; ?>
+    <?php include 'include/footer.php';?>
 
-<?php
-/** Verileri çekme Bölümü */
-$kitaplar = $db->query("SELECT * from kitaplar",PDO::FETCH_OBJ)->fetchAll();
-?>
-
-<?php
-include 'includes/header.php';
-?>
-
-<div class="container">
-    <h2 class="text-center">Kitap Listesi</h2>
-    <hr>
-    <div class="row">
-        <?php foreach ($kitaplar as $product) { ?>
-        <div class="col"><div class="card" style="width: 15rem;">
-                <img src="images/<?php echo $product->resim_url; ?>" class="card-img-top" alt="<?php echo $product->kitap_ad; ?>">
-                <div class="card-body">
-                    <h5 class="card-title"><?php echo $product->kitap_ad; ?></h5>
-                    <p class="card-text"><?php echo $product->yazar_ad; ?></p>
-                    <a href="#" class="btn btn-primary"><i class="fas fa-thumbs-up"></i></a>
-                    <a href="#" class="btn btn-primary"><i class="fas fa-thumbs-down"></i></a>
-                    <button kitap-id="<?php echo $product->kitap_id; ?>" class="btn btn-primary addToCartBtn">Ödünç Al</button>
-                </div>
-            </div>
-        </div>
-        <?php } ?>
-    </div>
-<script src="js/jquery-3.4.1.min.js"></script>
-<script src="js/bootstrap.min.js"></script>
-<script src="js/main.js"></script>
-</body>
-        
-<?php
-include 'includes/footer.php';
-?>
+  </body>
 </html>
+ 
